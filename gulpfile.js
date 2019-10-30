3c12ae0acfc0d0414cdbfcf6e888f1f5e81f5eb1
guanqirui <guanqirui@zju.edu.cn>
2019-10-30 20:59:15 +0800

    Update netlify.toml

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-const gulp = require("gulp");
+var gulp = require('gulp');
+// Plugins模块获取
+var minifycss = require('gulp-minify-css');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+// 生成sw.js
 const workbox = require("workbox-build");
 const uglifyes = require('uglify-es');
 const composer = require('gulp-uglify/composer');
@@ -11,7 +16,7 @@ gulp.task('generate-service-worker', () => {
         swDest: './public/sw.js',
         globDirectory: './public',
         globPatterns: [
-            "**/*.{html,css,js,json,woff2}"
+            "**/*.{html,css,js,json,woff2,jpg,png,svg,gif}"
         ],
         modifyURLPrefix: {
             "": "./"
@@ -24,7 +29,29 @@ gulp.task("uglify", function () {
         gulp.src("./public/sw.js"),
         uglify(),
         gulp.dest("./public")
-    );
+  );
 });
-
+// 执行 gulp build 命令时执行的任务
 gulp.task("build", gulp.series("generate-service-worker", "uglify"));
+
+// 压缩 public 目录 css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+});
+// 压缩 public 目录 html
+gulp.task('minify-html', function() {
+	return gulp.src('./public/**/*.html')
+		.pipe(htmlclean())
+		.pipe(htmlmin({
+			removeComments: true,
+			minifyJS: true,
+			minifyCSS: true,
+			minifyURLs: true,
+    }))
+    .pipe(gulp.dest('./public'))
+});
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css')
+));

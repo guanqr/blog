b1049d6684dab8fcf3ce58b00ec9d0cec96f9b0f
guanqirui <guanqirui@zju.edu.cn>
2020-09-11 17:03:18 +0800

    测试：去除繁琐的 sw 生成步骤

--- gulpfile.js
+++ gulpfile.js
@@ -31,4 +31,3 @@ gulp.task("uglify", function () {
 
 // 执行 gulp 命令时执行的任务
 gulp.task("build", gulp.series("generate-service-worker", "uglify"));
-gulp.task("sw-uglify", gulp.series("uglify"));
--- workbox/README.md
+++
@@ -1,3 +0,0 @@
-# WorkBox
-
-该文件夹中的文件用于构建 PWA，具体操作参考：<https://guanqr.com/tech/website/realize-pwa/>。
